32f10876f74ae93bca25b150e299d5be1d6e2f74
Anders Gade <agade@grundfos.com>
2019-11-05 12:23:40 +0100

    Updates

---
+++ precache-manifest.408ca916b639d41ec162e65c1ebd785d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bc3d19b22d939795b41411017d5fee3",
+    "url": "/steelCurio/index.html"
+  },
+  {
+    "revision": "5e43f1f7831bbda8a313",
+    "url": "/steelCurio/static/css/main.4f20d35c.chunk.css"
+  },
+  {
+    "revision": "5501bc57459b6da83282",
+    "url": "/steelCurio/static/js/2.f9c057b3.chunk.js"
+  },
+  {
+    "revision": "5e43f1f7831bbda8a313",
+    "url": "/steelCurio/static/js/main.17920508.chunk.js"
+  },
+  {
+    "revision": "de4ff38d1681a5527db3",
+    "url": "/steelCurio/static/js/runtime-main.33cde5b1.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/steelCurio/static/media/logo.25bf045c.svg"
+  }
+]);
