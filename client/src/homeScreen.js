3af9580d785e81e4b611aece349a9c49871cf6ae
Anders Gade <agade@grundfos.com>
2019-11-05 10:22:13 +0100

    WiP: Updates to A2HS

--- client/src/homeScreen.js
+++ client/src/homeScreen.js
@@ -1,5 +1,14 @@
 export const promptInstallation = () => {
   window.addEventListener('beforeinstallprompt', e => {
     console.log('prompt', e);
+    e.prompt();
+    e.userChoice
+      .then(choice => {
+        if (choice.outcome === 'accepted') {
+          console.log('User accepted the A2HS prompt');
+        } else {
+          console.log('User dismissed the A2HS prompt');
+        }
+      });
   });
 };
