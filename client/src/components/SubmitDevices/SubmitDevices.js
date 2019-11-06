e920ff72870e58b7ef7e7cec47aa2270db11017a
Anders Gade <agade@grundfos.com>
2019-11-06 17:56:03 +0100

    Updated the email functionality

--- client/src/components/SubmitDevices/SubmitDevices.js
+++ client/src/components/SubmitDevices/SubmitDevices.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import useDevices from '../../hooks/useDevices';
 import './SubmitDevices.css';
 
 const SubmitDevices = () => {
   const [state] = useDevices();
+  const [email, setEmail] = useState(null);
+  const [url, setUrl] = useState('http://www.angelfire.com/dc/html-webmaster/mailto.htm');
+  const subject = 'New GiM devices has been commissioned for you';
+  const body = 'Hi, click this link to claim your newly commissioned devices:'
+  const mailString = `mailto:${email}?subject=${subject}&body=${body} ${url}`;
+
+  const onClick = () => {
+    window.location.href = mailString;
+  }
 
   return (
     <div className="submit-devices">
@@ -14,13 +23,19 @@ const SubmitDevices = () => {
       <div>Or add devices to another person:</div>
       <div className="submit-devices__email">
         <label className="device-list__label">Email: </label>
-        <input className="device-list__name" type="email" />
+        <input
+          className="device-list__name"
+          type="email"
+          onChange={e => setEmail(e.target.value)}
+          disabled={!state.length}
+        />
       </div>
       <div>
-        <a
+        <button
           className="App__install-button"
-          href="mailto:no-one@snai1mai1.com"
-        >Send</a>
+          onClick={onClick}
+          disabled={!state.length || !email}
+        >Send</button>
       </div>
     </div>
   );
